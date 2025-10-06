e23cc4ed78678889a31d910d27646d526820b971
Hannes Achleitner <hannes.software@gmx.at>
2025-10-06 21:31:41 +0200

    Changelog by Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     `kotlin-dsl`
+    kotlin("plugin.serialization") version "2.2.20"
 }
 
 repositories {
     mavenCentral()
+    google()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 }
