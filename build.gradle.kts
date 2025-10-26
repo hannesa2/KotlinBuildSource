f595c91a09a60736e9d48f80e9378653fa96f514
Hannes Achleitner <hannes.software@gmx.at>
2025-10-26 19:37:01 +0100

    Remove 1.8 compability warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `kotlin-dsl`
     kotlin("plugin.serialization") version "2.2.20"
@@ -10,4 +14,13 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        // A good modern default. Use a version that matches your project's Kotlin plugin version.
+        jvmTarget.set(JvmTarget.JVM_17)
+        languageVersion.set(KotlinVersion.KOTLIN_2_0)
+        apiVersion.set(KotlinVersion.KOTLIN_2_0)
+    }
 }
